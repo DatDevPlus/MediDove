62d4df63dfeac1f942becd78ca1676f887a5b491
DatDevPlus <dat.phung_dp05@devplus.edu.vn>
2023-04-06 04:42:01 +0700

    feat: dpm-create-footer-home

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from '../components/Navbar/Navbar.jsx'
 import Footer from '../components/Footer/Footer.jsx'
 
+import HomeFooter from '../components/HomeFooter/HomeFooter.jsx'
 const HomePage = () => {
   return (
     <>
       <Navbar />
       <Footer />
+      <HomeFooter />
     </>
   )
 }
