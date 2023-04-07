5c19e0327f1b5bafb34192bf87fd95f63a4da3fb
DatDevPlus <dat.phung_dp05@devplus.edu.vn>
2023-04-07 11:35:24 +0700

    feat: dpm-rebase-footer

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar.jsx'
-import Footer from '../components/Footer/Footer.jsx'
-
-import HomeFooter from '../components/HomeFooter/HomeFooter.jsx'
 import RemainingFooter from '../components/RemainingFooter/RemainingFooter'
+import HomeFooter from '../components/HomeFooter/HomeFooter.jsx'
 const HomePage = () => {
   return (
     <>
       <Navbar />
-      <Footer />
       <HomeFooter />
-      <RemainingFooter/>
+      <RemainingFooter />
     </>
   )
 }
