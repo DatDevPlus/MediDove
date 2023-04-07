801631db34be758073752bba9a41340d0afb90fc
tunglqgithub <tung.le_dp05@devplus.edu.vn>
2023-04-07 10:12:07 +0700

    feat: dpm-fix-reponsive

---
+++ src/components/Department/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Department.scss"
+
+
+const Department = () => {
+  return (
+   <div className='department'>
+    <div className='container'>
+
+    </div>
+   </div>
+  )
+}
+
+export default Department
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Department from '../components/Department/HomePage';
 import OurTeam from '../components/OurTeam/OurTeam';
 
 
 const HomePage = () => {
   return (
    <>
+   <Department/>
     <OurTeam/>
    </>
   )
