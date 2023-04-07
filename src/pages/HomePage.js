c7cf358b295566071a8f77faf9625183c502a8fd
tunglqgithub <tung.le_dp05@devplus.edu.vn>
2023-04-07 12:31:03 +0700

    feat: fix-error-in-design

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,15 +4,16 @@ import RemainingFooter from "../components/RemainingFooter/RemainingFooter";
 import HomeFooter from "../components/HomeFooter/HomeFooter.jsx";
 import OurTeam from "../components/OurTeam/OurTeam";
 import AboutUs from "../components/AboutUs/AboutUs";
+import Department from "../components/Department/Department";
 
 const HomePage = () => {
   return (
     <>
       <Navbar />
-      <OurTeam />
       <AboutUs />
+      <Department />
+      <OurTeam />
       <HomeFooter />
-      <RemainingFooter />
     </>
   );
 };
